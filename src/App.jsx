2b258448053e3048f0bdab2d4a8ebf62e2a3d461
Mamadou Alpha Diallo <mamadou-alpha.diallo.2@etu.u-bordeaux.fr>
2026-08-07 19:04:26 +0200

    chore: mise en place de l'architecture du projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="text-3xl font-bold text-blue-600">Test Tailwind</h1>
     </>
   )
 }
